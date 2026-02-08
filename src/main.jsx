e2a17a371b262dfa52e09b5e4806aa5f873e088a
I-am-nagesh <i.am.nageshpandey@gmail.com>
2026-02-08 15:10:09 +0530

    google analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css'; 
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+if (import.meta.env.PROD) {
+  const gaId = import.meta.env.VITE_GA_ID;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+  if (gaId) {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", gaId);
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
 );
